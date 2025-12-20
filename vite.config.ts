2328f2ed64f6fdbd423dcf524f7c6a60c3a52d41
Ademir José Ferreira Júnior <ademirfel.ferreira@gmail.com>
2025-12-19 22:49:21 -0300

    chore: ignore ts build info, prevent ts emit, remove vite plugin type warning

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import path from "node:path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import path from "node:path";
 import { defineConfig } from "vite";
 import wasm from "vite-plugin-wasm";
 
 export default defineConfig(() => ({
+  // @ts-expect-error vite-plugin-wasm type mismatch
   plugins: [react(), tailwindcss(), wasm()],
   resolve: {
     alias: {
