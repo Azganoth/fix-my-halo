fd069f12b81fe8313cdd494df85bf6aeb46c0bd5
Ademir José Ferreira Júnior <ademirfel.ferreira@gmail.com>
2025-12-21 21:35:30 -0300

    feat: add analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
+import { Analytics } from "@vercel/analytics/next";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <Analytics />
     <App />
   </StrictMode>,
 );
